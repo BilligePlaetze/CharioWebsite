87581c661518c5a54c25a7c1c9ff2e2ba0fb1f9e
Dominik Juelg <djuelg@united-internet.de>
2018-03-24 19:49:25 +0100

    added materialize to project; added first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
+import { WorldComponent } from './components/world/world.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
